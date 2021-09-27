6d25c338bf64efcc6835e5962c67b404b53a33ca
MainSilent <hamidreza.neo@gmail.com>
2021-09-27 19:43:39 +0330

    simple columns

--- src/shows/list.js
+++ src/shows/list.js
@@ -3,19 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/fontawesome-free-solid'
 
 class List extends Component {
-    componentDidMount() {
-        const list = document.querySelector(".list");
-
-        list.addEventListener("wheel", e => {
-            list.scrollLeft += e.deltaY
-        })
-    }
     render() {
         return (
             <div className="list">
-                {Array(50).fill().map((a, i) => 
+                {Array(5).fill().map((a, i) => 
                     <div className="show" key={i}>
-                        <img className="thumbnail" alt={`Sample ${i}`} src={`./static/sample/sample${Math.floor(Math.random() * (6 - 1) + 1)}.png`}/>
+                        <img className="thumbnail" alt={`Sample ${i}`} src={`./static/sample/sample${++i}.png`}/>
                         <div className="details">
                             <h2 className="title">Sample {++i}</h2>
                             <p className="views">{Math.floor(Math.random() * (100 - 10) + 10)} <FontAwesomeIcon icon={faEye} /></p>
