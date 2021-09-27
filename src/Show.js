27e9469e88fb70626780322d562fc3f563e200de
MainSilent <hamidreza.neo@gmail.com>
2021-09-27 23:02:36 +0330

    add host info

--- src/Show.js
+++ src/Show.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/fontawesome-free-solid'
 import './scss/show.scss'
 
 class Show extends Component {
@@ -10,7 +12,12 @@ class Show extends Component {
 
         return (
             <div className="show-container">
-                <div className="host-container"></div>
+                <div className="host-container">
+                    <img src='/static/images/adobe.jpg' alt="adobe logo"/>
+                    <h1>Adobe<span><br/>Live show started: 8:53 AM</span></h1>
+                    {/* <p className="views">27 <FontAwesomeIcon icon={faEye} /></p> */}
+                </div>
+
                 <div className="product-container"></div>
                 <div className="video-container"></div>
                 <div className="comments-container"></div>
