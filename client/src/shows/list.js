e1d5ca2bbb29cc22559eafbb763f54097202b2bc
MainSilent <hamidreza.neo@gmail.com>
2021-09-27 14:16:52 +0330

    column shows

--- client/src/shows/list.js
+++ client/src/shows/list.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class List extends Component {
+    componentDidMount() {
+        const list = document.querySelector(".list");
+
+        list.addEventListener("wheel", e => {
+            list.scrollLeft += e.deltaY
+        })
+    }
     render() {
         return (
             <div className="list">
-                <h1>List</h1>
+                {Array(100).fill().map((a, i) => 
+                    <div className="show">
+                        <h1>Cool {i}</h1>
+                    </div>
+                )}
             </div>
         )
     }
