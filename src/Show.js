977c121562ed37e6935bb9a1b1e8d1e909c1377c
MainSilent <hamidreza.neo@gmail.com>
2021-09-27 21:06:14 +0330

    change show style

--- src/Show.js
+++ src/Show.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import { withRouter } from "react-router-dom"
 import './scss/show.scss'
 
 class Show extends Component {
     render() {
+        const i = window.location.pathname.split('').pop()
+        if (isNaN(i) || i < 1 || i > 5)
+            this.props.history.push("/404")
+
         return (
             <div className="show-container">
-                <h1>{window.location.pathname}</h1>
+                <div className="host-container"></div>
+                <div className="product-container"></div>
+                <div className="video-container"></div>
+                <div className="comments-container"></div>
             </div>
         )
     }
 }
 
-export default Show
+export default withRouter(Show)
