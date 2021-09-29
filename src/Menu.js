ba4bca3c5e2404a5e8f8e9c3aada4c69793ab658
MainSilent <hamidreza.neo@gmail.com>
2021-09-29 10:17:45 +0330

    add menu sidebar

--- src/Menu.js
+++ src/Menu.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react'
 import { NavLink as Link } from "react-router-dom"
 import './scss/menu.scss'
 
+function Items() {
+    return (
+        <>
+            <Link to="/" exact><li>Home</li></Link>
+            <Link to="/shows"><li>Shows</li></Link>
+            <Link to="/login" isActive={(m, l) => '/login' === l.pathname.toLowerCase() || '/register' === l.pathname.toLowerCase()}>
+                <li>Login / Register</li>
+            </Link>
+        </>
+    )
+}
+
 class Menu extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isOpen: false
+        }
+    }
+    componentDidMount() {
+
+    }
     render() {
         return (
             <div className='menu'>
                 <a href='/'><img className="logo" src="/static/images/logo.png" alt="logo" /></a>
                 <ul className="items">
-                    <Link to="/" exact><li>Home</li></Link>
-                    <Link to="/shows"><li>Shows</li></Link>
-                    <Link to="/login" isActive={(m, l) => '/login' === l.pathname.toLowerCase() || '/register' === l.pathname.toLowerCase()}>
-                        <li>Login / Register</li>
-                    </Link>
+                    <Items />
+                </ul>
+                {/* Responsive Menu */}
+                <span className="hamberger-btn" onClick={() => this.setState({ isOpen: !this.state.isOpen })}>&#9776;</span>
+                <ul className="items-rwd" style={this.state.isOpen ? { width: '300px' } : {}}>
+                    <span className="closebtn" onClick={() => this.setState({ isOpen: false })}>&times;</span>
+                    <Items />
                 </ul>
             </div>
         )
