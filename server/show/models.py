a3a0fad39bb60d356e06bf5eb92391488a118954
MainSilent <hamidreza.neo@gmail.com>
2021-10-12 22:01:34 +0330

    add show model

--- server/show/models.py
+++ server/show/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=128)
+    product_name = models.CharField(max_length=128)
+    product_url = models.URLField(max_length=1024)
+    product_price = models.IntegerField()
+    product_description = models.TextField(max_length=1024)
