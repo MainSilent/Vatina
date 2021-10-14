bad4da9716e9b56acbfd667b262f891267ff42b6
MainSilent <hamidreza.neo@gmail.com>
2021-10-14 18:16:19 +0330

    fix a bug

--- src/dashboard/show-details.js
+++ src/dashboard/show-details.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
 
 class ShowDetails extends Component {
-    render() {
-        const show = this.props.ShowsList.filter(s => s.playback_id === this.props.location.pathname.split('/').pop())[0]
-        if (!show)
+    constructor() {
+        super()
+        this.state = {
+            show: {}
+        }
+    }
+    setShow() {
+        if (this.props.ShowsList.length === 0)
             this.props.history.push('/dashboard')
 
+        const newShow = this.props.ShowsList.filter(s => s.playback_id === this.props.location.pathname.split('/').pop())[0]
+        newShow !== this.state.show &&
+            this.setState({
+                show: newShow
+            })
+    }
+    componentDidMount() {
+        this.setShow()
+    }
+    componentDidUpdate() {
+        this.setShow()
+    }
+    render() {
         return (
             <div>
-                <p>Stream Key: {show.stream_key}</p>
+                <p>Stream Key: {this.state.show.stream_key}</p>
                 <p>RTMP Server URL: rtmps://global-live.mux.com:443/app</p>
                 <hr/>
-                <p>Show Link: <a href={`/show/${show.playback_id}`}>{window.location.origin}/show/{show.playback_id}</a></p>
+                <p>Show Link: <a href={`/show/${this.state.show.playback_id}`}>{window.location.origin}/show/{this.state.show.playback_id}</a></p>
             </div>
         )
     }
